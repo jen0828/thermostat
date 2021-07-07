2c51a84759e05e88da4bb7458ff90901fe8900c1
Jenny Moon <jenny0moon@gmail.com>
2021-07-07 11:22:41 +0100

    Test: power saving mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -29,5 +29,22 @@ describe('Thermostat', () => {
     expect(thermostat.getCurrentTemperature()).toEqual(10);
   });
 
+  it('has power saving mode on by default', () =>{
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  it('can switch PSM off', () => {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+
+  it('can switch PSM back on', () => {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+
 });
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,6 +3,7 @@ class Thermostat {
   constructor() {
     this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
+    this.powerSavingMode = true;
   }
   getCurrentTemperature() {
     return this.temperature;
@@ -19,6 +20,18 @@ class Thermostat {
 
 isMinimumTemperature() {
   return this.temperature === this.MINIMUM_TEMPERATURE;
+}
+
+isPowerSavingModeOn() {
+  return this.powerSavingMode === true;
+}
+
+switchPowerSavingModeOff() {
+  this.powerSavingMode = false;
+}
+
+switchPowerSavingModeOn() {
+  this.powerSavingMode = true;
 }
 
 };
