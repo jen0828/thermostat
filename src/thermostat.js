8ff16fc6435cdf3cf267cce9c5cdf9f74496ca0c
Jenny Moon <jenny0moon@gmail.com>
2021-07-07 12:05:12 +0100

    Test: energy usage function

--- src/thermostat.js
+++ src/thermostat.js
@@ -3,6 +3,8 @@ class Thermostat {
   constructor() {
     this.MINIMUM_TEMPERATURE = 10;
     this.DEFAULT_TEMPERATURE = 20;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
     this.temperature = this.DEFAULT_TEMPERATURE;
     this.powerSavingMode = true;
     this.MAX_LIMIT_PSM_ON = 25;
@@ -51,6 +53,16 @@ isMaximumTemperature() {
 }
 resetTemperature() {
   this.temperature = this.DEFAULT_TEMPERATURE;
+}
+
+energyUsage() {
+  if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+    return 'low-usage';
+  }
+  if (this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
+    return 'medium-usage';
+  }
+  return 'high-usage';
 }
 
 };
